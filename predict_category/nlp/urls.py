a171fc75684694d79289cbf15d462c1d004eddce
nguyenvandai61 <nguyenvandai61@users.noreply.github.com>
2021-03-07 17:40:21 +0700

    Segment many files
    Remove html tags

--- predict_category/nlp/urls.py
+++ predict_category/nlp/urls.py
@@ -2,6 +2,14 @@ import os
 
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 PARENT_PATH = os.path.abspath(os.path.join(DIR_PATH, os.pardir))
+
 DICTIONARY_PATH = os.path.join(PARENT_PATH, "dictionary")
 STOP_WORDS = os.path.join(DICTIONARY_PATH, "stop_words3.txt")
+
+RAW_DATA_PATH = os.path.join(PARENT_PATH, "raw_data")
+RAW_DATA_TEST_PATH = os.path.join(RAW_DATA_PATH, "test")
+
+EXAM1 = os.path.join(RAW_DATA_TEST_PATH, "example1.txt")
+
 print(STOP_WORDS)
+print(EXAM1)
