70abfff282eeec6ff6075daafdb6936b67fe4a87
Can Le <canleit5509@gmail.com>
2021-03-15 01:49:55 +0700

    Play with Json

--- feature_extraction/settings.py
+++ feature_extraction/settings.py
@@ -5,8 +5,9 @@ DATA_TRAIN_PATH = os.path.join(DIR_PATH, 'train/')
 DATA_TEST_PATH = os.path.join(DIR_PATH, 'test/')
 DATA_TRAIN_JSON = os.path.join(DIR_PATH, 'data_train.json')
 DATA_TEST_JSON = os.path.join(DIR_PATH, 'data_test.json')
+DATA_RAW = os.path.join(DIR_PATH, 'raw_data/')
 STOP_WORDS = os.path.join(DIR_PATH, 'stop_words3.txt')
 SPECIAL_CHARACTER = '0123456789%@$.,=+-!;/()*"&^:#|\n\t\''
 DICTIONARY_PATH = 'dictionary.txt'
 
-print(STOP_WORDS)
+print(DATA_RAW)
